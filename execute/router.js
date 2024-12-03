12c12200d46281a81a01076d3bafedc0d4603dd7
JjeongBright <wjswjdgus333@gmail.com>
2024-12-04 00:15:55 +0900

    feat: csv 파일 업로드 페이지 생성 및 결과 출력 페이지 생성

--- execute/router.js
+++ execute/router.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const router = express.Router();
 
-// 라우트 파일 불러오기
 const loginRoutes = require("../controller/login");
 const introRoutes = require("../controller/intro");
 const signupRoutes = require("../controller/signup");
+const uploadRoutes = require("../util/upload"); // 업로드 라우트 추가
 
-// 라우트 통합
 router.use("/", loginRoutes);
 router.use("/", introRoutes);
 router.use("/", signupRoutes);
+router.use("/", uploadRoutes); // 업로드 라우트 적용
 
 module.exports = router;
